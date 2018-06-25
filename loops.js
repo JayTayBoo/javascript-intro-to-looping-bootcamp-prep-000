6a418f2dea0778749236b410d1cff3a760599361
Jacob Boodry <jake.boodry@live.com>
2018-06-25 00:10:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,6 +18,7 @@ function doWhileLoop(array){
   return Math.random() >= 0.5}
   
   do { 
-  array.pop()}
-  while array.length > 0 && maybeTrue()
+  array.pop()} 
+  while (array.length > 0 && maybeTrue())
+  return array
 }
